fb0d1915feabf02e4430cbd94ac328a2900e7090
Zhanibek Kakharov <zhanibek.kakharov+pearson@pearson.com>
2026-04-16 20:39:29 +0500

    Initial implementation

---
+++ .NET Fundamentals/HelloUserClassLibrary/HelloUserConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+using HelloUserConsoleApp.Services;
+using HelloUserLibrary;
+
+namespace HelloUserConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var userName = args.Length > 0 ? string.Join(" ", args) : string.Empty;
+            var greeterService = new GreeterService();
+            greeterService.Greet(userName);
+        }
+    }
+}
