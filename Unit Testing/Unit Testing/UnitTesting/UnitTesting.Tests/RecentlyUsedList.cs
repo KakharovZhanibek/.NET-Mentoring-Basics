b5dfdc46faf9dd03e15278aeff04c975d46fd466
Zhanibek Kakharov <zhanibek.kakharov+pearson@pearson.com>
2026-06-10 21:39:08 +0500

    Add test for empty list having zero count

---
+++ Unit Testing/Unit Testing/UnitTesting/UnitTesting.Tests/RecentlyUsedList.cs
@@ -0,0 +1,13 @@
+﻿namespace UnitTesting.Tests
+{
+    public class RecentlyUsedList
+    {
+        [Fact]
+        public void NewList_HasCountOfZero()
+        {
+            var list = new UnitTesting.Models.RecentlyUsedList();
+
+            Assert.Equal(0, list.Count);
+        }
+    }
+}
