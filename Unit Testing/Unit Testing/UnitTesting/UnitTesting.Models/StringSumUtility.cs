4315bd6f9f594afbf7f056c1932a51e7fe9113c4
Zhanibek Kakharov <zhanibek.kakharov+pearson@pearson.com>
2026-06-11 00:21:54 +0500

    [Green] Implement parsing and addition of natural numbers

--- Unit Testing/Unit Testing/UnitTesting/UnitTesting.Models/StringSumUtility.cs
+++ Unit Testing/Unit Testing/UnitTesting/UnitTesting.Models/StringSumUtility.cs
@@ -10,7 +10,10 @@ namespace UnitTesting.Models
     {
         public string Sum(string num1, string num2)
         {
-            return "0";
+            int n1 = int.TryParse(num1, out int v1) && v1 >= 0 ? v1 : 0;
+            int n2 = int.TryParse(num2, out int v2) && v2 >= 0 ? v2 : 0;
+
+            return (n1 + n2).ToString();
         }
     }
 }
