6605d5b7bd72500b8edfe0a815bcf55a3ac8ba3a
Zhanibek Kakharov <zhanibek.kakharov+pearson@pearson.com>
2026-06-11 00:45:58 +0500

    [Green] Return Buzz for multiples of five

--- Unit Testing/Unit Testing/UnitTesting/UnitTesting.Models/FizzBuzz.cs
+++ Unit Testing/Unit Testing/UnitTesting/UnitTesting.Models/FizzBuzz.cs
@@ -11,6 +11,7 @@ namespace UnitTesting.Models
         public string GetResult(int number)
         {
             if (number % 3 == 0) return "Fizz";
+            if (number % 5 == 0) return "Buzz";
 
             return number.ToString();
         }
