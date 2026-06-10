03f3c88dd539fdf07c26c873a6e314e86f02f541
Zhanibek Kakharov <zhanibek.kakharov+pearson@pearson.com>
2026-06-11 00:44:13 +0500

    [Red] Add test for plain number returning its string value

---
+++ Unit Testing/Unit Testing/UnitTesting/UnitTesting.Tests/FizzBuzzTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTesting.Models;
+using Xunit;
+
+namespace UnitTesting.Tests
+{
+    public class FizzBuzzTests
+    {
+        [Fact]
+        public void PlainNumber_ReturnsNumberAsString()
+        {
+            var fizzBuzz = new FizzBuzz();
+
+            var result = fizzBuzz.GetResult(1);
+
+            Assert.Equal("1", result);
+        }
+    }
+}
